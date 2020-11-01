af6ae9a5bdc9b120e7dbc69fb804bc48e28829ac
Tauno Erik <taunoerik@pop-os.localdomain>
2020-11-01 22:11:53 +0200

    radar + LED stripe

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@
 #include <Adafruit_HTU21DF.h>     // HTU21
 #include <Adafruit_CCS811.h>      // CCS811
 #include <ThingSpeak.h>
+#include <Adafruit_NeoPixel.h>
 #include "Tauno_secrets.h"        // Passwords
 
 /* Enable debug info Serial print */
@@ -47,6 +48,22 @@ Adafruit_HTU21DF Htu = Adafruit_HTU21DF();
 /* CCS811 */
 Adafruit_CCS811 Ccs;
 
+/* RCWK-0516 */
+const int RADAR_PIN {14};  // D5
+
+/* NeoPixel stripe */
+const int PIXEL_PIN {12};  // D6
+const int PIXEL_COUNT {8};
+Adafruit_NeoPixel pixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
 /* millis() */
 uint32_t prev_measure_ms {};
 const uint MEASURE_INTERVAL {60*1000};  // 60s
@@ -64,6 +81,35 @@ float htu21_hum {};
 int ccs811_eCO2 {};
 int ccs811_TVOC {};
 int display_sequence {};
+
+/********************************************/
+void pixel_rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  for (long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    for (int i = 0; i < pixel.numPixels(); i++) {  // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (strip.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / pixel.numPixels());
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      pixel.setPixelColor(i, pixel.gamma32(pixel.ColorHSV(pixelHue)));
+    }
+    pixel.show();  // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
+
+void pixel_off() {
+  pixel.clear();
+  pixel.show();
+}
 
 /********************************************/
 
@@ -128,10 +174,27 @@ void setup() {
 
   /* ThingSpeak setup */
   ThingSpeak.begin(client);
+
+  /* */
+  pinMode(RADAR_PIN, INPUT);
+
+  /* NeoPixel stripe*/
+  pixel.begin();
+  pixel.show();  // Turn OFF all pixels ASAP
+  pixel.setBrightness(50);  // Set BRIGHTNESS to about 1/5 (max = 255)
 }
+
 
 void loop() {
   uint32 current_ms = millis();
+
+  bool motion = digitalRead(RADAR_PIN);
+
+  if (motion) {
+    // DEBUG_PRINT("Motion ");
+    pixel_rainbow(10);
+    pixel_off();
+  }
 
   if ((current_ms - prev_display_ms) >= DISPLAY_CHANGE_INTERVAL) {
     is_display_change_time = true;
